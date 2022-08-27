03757cb9ec188c31edf93d02636e9735bea1c812
Marco Spicuzza <marcospicuzza00@gmail.com>
2022-08-27 15:44:29 +0200

    added setInterval event at the load of page

--- footer.js
+++ footer.js
@@ -3,7 +3,7 @@ const btnText = document.querySelector(".scoreboard-btn_text")
 const firstStats = document.querySelectorAll(".heading-stats_container");
 const secondaryStats = document.querySelectorAll(".secondary-stats");
 
-statsBtn.addEventListener("click", function changeStats() {
+statsBtn.addEventListener("click", changeStats = () => {
     if (btnText.textContent === 'Syrup Pools') {
         btnText.textContent = 'Farms'
     } else if (btnText.textContent = 'Farms') {
@@ -21,3 +21,29 @@ statsBtn.addEventListener("click", function changeStats() {
         } else secondaryStats.forEach(element => element.classList.add('hidden-table_secondary'))
     })
 });
+
+changeStatsOnLoad = () => {
+    if (btnText.textContent === 'Syrup Pools') {
+        btnText.textContent = 'Farms'
+    } else if (btnText.textContent = 'Farms') {
+        btnText.textContent = 'Syrup Pools'
+    }
+    firstStats.forEach(element => {
+        if(element.classList.contains("hidden-table")) {
+            firstStats.forEach(element => element.classList.remove('hidden-table'))
+        } else firstStats.forEach(element => element.classList.add("hidden-table"))
+    })
+
+    secondaryStats.forEach(element => {
+        if(element.classList.contains("hidden-table_secondary")) {
+            secondaryStats.forEach(element => element.classList.remove('hidden-table_secondary'))
+        } else secondaryStats.forEach(element => element.classList.add('hidden-table_secondary'))
+    })
+    console.log("ciao");
+}
+
+window.addEventListener ? 
+window.addEventListener("load", changeStatsOnLoad(),false) : 
+window.attachEvent && window.attachEvent("onload",changeStatsOnLoad());
+
+setInterval('changeStatsOnLoad()', 3000)
